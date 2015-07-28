5673162dc43ffbb2a0807bd2f961406747597e7d
Yogesh Ramola <yogeshramola@gmail.com>
2015-07-28 16:42:44 -0700

    Added Middle tier code

---
+++ MiddleTier/ProcessWifiThermometerData/DataModel/DeviceTemperature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WifiThermometer.DataModel
+{
+    public class DeviceTemperature
+    {
+        public string DeviceId { get; set; }
+
+        public string DeviceName { get; set; }
+
+        public double Temperature { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+}
